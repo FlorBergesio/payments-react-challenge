050efeacaa1c769aeb93c21c37be05aaa3295480
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2022-06-07 21:30:34 -0300

    Defined logic for displaying PaidPayments

---
+++ components/PaidPayments.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export default function PaidPayments( {student_orders} ) {
+  return (
+    <>
+      <div className="flex justify-between gap-4">
+        <h2 className='font-bold'>Cuotas pendientes</h2>
+        <span>V</span>
+      </div>
+      <div>
+        {
+          student_orders.map(order => (
+            <div className="flex justify-between gap-4 my-2">
+              <div className="text-left">
+                <h3 className="text-lg">{order.name}</h3>
+                <span className="text-sm">Pagado el {`${new Date(order.payin.created).getDate()} de ${new Date(order.payin.created).toLocaleString('default', { month: 'short' })}.`}</span>
+              </div>
+              <div className="text-right">
+                →
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </>
+  );
+}
